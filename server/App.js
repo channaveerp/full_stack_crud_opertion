2cf06796d5e84c4a6d2fdd8cf28fd49f16bb3505
channaveer <channaveerpatil999@gmail.com>
2023-07-30 15:08:22 +0530

    error changes

--- server/App.js
+++ server/App.js
@@ -23,8 +23,8 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'./client/build/index.html')
-})
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 
