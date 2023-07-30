41f603293b0e498a5eeb0d5b5eec7a025c99677a
channaveer <channaveerpatil999@gmail.com>
2023-07-30 18:11:50 +0530

    package changes done

--- server/App.js
+++ server/App.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 // static files accessible
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
